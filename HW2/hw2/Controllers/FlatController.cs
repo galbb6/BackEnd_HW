55d84f1ea8bf4fb5ee339ac6d8d9348a2030a11d
galbb6 <116492049+galbb6@users.noreply.github.com>
2022-12-22 15:58:23 +0200

    flats and user
    all ready for user and flats

---
+++ HW2/hw2/Controllers/FlatController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AirBnb_Part_2.Models;
+using System.Net;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AirBnb_Part_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FlatController : ControllerBase
+    {
+        //--------------------------------------------------------------------------------------------------
+        // # GET FLATS                                
+        //--------------------------------------------------------------------------------------------------
+        // GET: api/<FlatController>
+        [HttpGet("Get All Flats")]
+        public List<Flat> Get()
+        {
+            return Flat.Read();
+        }
+
+
+        //--------------------------------------------------------------------------------------------------
+        // # GET flat by ID IF NOT FOUND RETURN NULL                              
+        //--------------------------------------------------------------------------------------------------
+        // GET api/<FlatController>/5
+        [HttpGet("Get Flat By {id}")]
+        public Flat Get(int id)
+        {
+             List< Flat > F = Flat.Read();
+            foreach (var item in F)
+            {
+                if (item.FlatId == id)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # GET ALL FLATS WHERE CITY == CITY AND PRICE <= PRICE                                    
+        //--------------------------------------------------------------------------------------------------
+        [HttpGet("Get Flats BY {City,Price}")]
+        public  List<Flat> GetByCityAndPrice(string city, double price)
+        {
+            List<Flat> FList =Flat.getCityPrice(city,price);
+
+                if (FList.Count()>0)
+                {
+                    return FList;
+                }
+            
+            return null;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # INSERT FLAT                              
+        //--------------------------------------------------------------------------------------------------
+
+        // POST api/<FlatController>
+        [HttpPost("Insert Flat")]
+        public bool Post([FromBody] Flat flat)
+        {
+            int temp = flat.InsertFlat(flat);
+            if (temp > 0)
+            {
+                return true;
+            }
+            else
+
+            {
+                return false;
+            }
+
+        }
+        //--------------------------------------------------------------------------------------------------
+        // # UPDATE FLAT                       
+        //--------------------------------------------------------------------------------------------------
+
+        // PUT api/<FlatController>/5
+        [HttpPut("Update Flat")]
+        public bool Put([FromBody] Flat flat)
+        {
+            int temp = flat.UpdateFlat(flat);
+            if (temp > 0)
+            {
+                return true;
+            }
+            else
+
+            {
+                return false;
+            }
+
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # DELETE FLAT                            
+        //--------------------------------------------------------------------------------------------------
+
+        [HttpDelete("Delete Flat/flatId/{flatId}")]
+        public bool Delete(int flatId)
+        {
+            Flat f = new Flat();
+            int temp = f.DeleteFlat(flatId);
+            if (temp > 0)
+            {
+                return true;
+            }
+            else
+
+            {
+                return false;
+            }
+        }
+
+    }
+}
