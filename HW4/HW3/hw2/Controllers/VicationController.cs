5bbd5072f23570d9cb653fbf374deb4100212abc
galbb6 <116492049+galbb6@users.noreply.github.com>
2023-01-19 18:52:56 +0200

    server side and data done

---
+++ HW4/HW3/hw2/Controllers/VicationController.cs
@@ -0,0 +1,133 @@
+﻿using AirBnb_Part_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AirBnb_Part_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VicationController : ControllerBase
+    {
+        //--------------------------------------------------------------------------------------------------
+        // # GET ALL VACATION                               
+        //--------------------------------------------------------------------------------------------------
+
+        // GET: api/<VixationController>
+        [HttpGet]
+        public List<Vacation> Get()
+        {
+            return Vacation.Read();
+        }
+        //--------------------------------------------------------------------------------------------------
+        // # GET VACATION  BY ID                             
+        //--------------------------------------------------------------------------------------------------
+        // GET api/<VixationController>/5
+
+        [HttpGet("id/{id}")]
+        public Vacation Get(int id)
+        {
+
+            List<Vacation> V = Vacation.Read();
+            foreach (var item in V)
+            {
+                if (item.id == id)
+                {
+                    return item;
+                }
+
+            }
+            return null;
+        }
+        //--------------------------------------------------------------------------------------------------
+        // # GET VACATION BY DATES                               
+        //--------------------------------------------------------------------------------------------------
+        [HttpGet("startDate/{startDate}/endDate/{endDate}")]
+        public List<Vacation> getByDates(DateTime startDate, DateTime endDate)
+        {
+            List<Vacation> VList = Vacation.getByDatesOrders(startDate, endDate);
+            
+                if (VList.Count>=0)
+                {
+
+                    return VList;
+                }
+
+            
+            return null;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # INSERT VACATION                               
+        //--------------------------------------------------------------------------------------------------
+        // POST api/<VixationController>
+        [HttpPost]
+        public bool Post([FromBody] Vacation V)
+
+        {
+
+            int temp = V.Insert(V);
+            if (temp > 0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # UPDATE VACATION                               
+        //--------------------------------------------------------------------------------------------------
+
+        // PUT api/<VixationController>/5
+        [HttpPut("id/{id}")]
+        public IActionResult Put(int id, [FromBody] Vacation vacation)
+        {
+            vacation.id = id;
+            int temp = vacation.UpdateVacation(vacation);
+            if (temp > 0)
+            {
+                return Ok();
+            }
+            else
+
+            {
+                return NotFound("id " + id.ToString() + " was not update");
+            }
+
+        }
+        //--------------------------------------------------------------------------------------------------
+        // # DELETE VACATION                               
+        //--------------------------------------------------------------------------------------------------
+        // DELETE api/<VixationController>/5
+        [HttpDelete("id/{id}")]
+        public bool Delete(int id)
+        {
+            Vacation v = new Vacation();
+            int temp = v.DeleteVacation(id);
+            if (temp > 0)
+            {
+                return true;
+            }
+            else
+
+            {
+                return false;
+            }
+
+
+        }
+       
+
+
+
+
+
+
+    }
+}
---
+++ HW4/HW3/hw2/Models/User.cs
@@ -0,0 +1,83 @@
+﻿
+
+namespace AirBnb_Part_2.Models
+{
+    public class UserProfile
+    {
+
+    
+        public int UserId { get; set; }
+        public string firstName { get; set; }
+        public string familyName { get; set; }
+        public string email { get; set; }
+        public string UserPassword { get; set; }
+        public bool isActive { get; set; }
+        public bool isAdmin { get ; set ; }
+        
+
+        private static List<UserProfile> UsersList = new List<UserProfile>();
+
+        //--------------------------------------------------------------------------------------------------
+        // # GET ALL USERS                           
+        //--------------------------------------------------------------------------------------------------
+        public static List<UserProfile> Read()
+        {
+            DBservices dbs = new DBservices();
+            UsersList = dbs.getUserFromDB();
+            return UsersList;
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # INSERT NEW USER                            
+        //--------------------------------------------------------------------------------------------------
+        public static int Insert(UserProfile profile)
+        {
+
+            DBservices dbs = new DBservices();
+            return dbs.InsertUserToDB(profile);
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # UPDATE USER PROFILE                          
+        //--------------------------------------------------------------------------------------------------
+
+        public static int UpdateUserProfile(UserProfile profile)
+        {
+            DBservices dbs = new DBservices();
+            return dbs.UpdateUserToDB(profile);
+
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # UPDATE USER ACTIVITY                     
+        //--------------------------------------------------------------------------------------------------
+
+        public static int setActive(string email, bool isActive )
+        {
+            DBservices dbs = new DBservices();
+            return dbs.setActiveDB(email,isActive);
+
+        }
+
+        //--------------------------------------------------------------------------------------------------
+        // # DELETE USER PROFILE                             
+        //--------------------------------------------------------------------------------------------------
+        public static int DeleteUserProfile(string email)
+        {
+            DBservices dbs = new DBservices();
+            return dbs.DeleteUserProfile(email);
+        }
+        //--------------------------------------------------------------------------------------------------
+        // # FIND USER PROFILE                             
+        //--------------------------------------------------------------------------------------------------
+        public UserProfile GetAccess(string email)
+        {
+            DBservices dbs = new DBservices();
+
+            return dbs.GetAccessFromDB(email);
+        }
+
+
+
+    }
+}
